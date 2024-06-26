0b4bd69b0d2209a158015643d9d6aadf702b007c
chingkamhing <chingkamhing@gmail.com>
2024-06-26 17:50:22 +0800

    Add block of required_providers{}

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,18 @@
 terraform {
   required_version = ">= 0.13"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "teraform.kamching"
     key            = "ec2/terraform.tfstate"
     region         = "ap-southeast-1"
+    encrypt        = true
     dynamodb_table = "terraform_ec2"
   }
 }
